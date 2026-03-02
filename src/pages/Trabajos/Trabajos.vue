f8708055519c9520af5f3c4273d7afadf72903ab
Axel Martínez <majose.mtnezgu1201@gmail.com>
2026-03-03 00:26:37 +0100

    Página de obras creada

--- src/pages/Trabajos/Trabajos.vue
+++ src/pages/Trabajos/Trabajos.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+import { obras } from './obrasDetalles';
+
+let ilustracion = obras.slice(0,10);
+let comic = obras.slice(11,13);
+let personajes = obras.slice(13,15);
+let diseno = obras.slice(15,18);
+let otros = obras.slice(18,19);
+
 
 </script>
 
@@ -7,24 +15,185 @@
         <h1 class="text-4xl text-center py-1.5">MIS TRABAJOS</h1>
     </div>
 
-    <section id="ilustracion" class="bg-amber-200 h-200">
-        <h1 class="text-xl"> Ilustración </h1>
+    <section id="ilustracion" class="bg-black text-white">
+        <h1 class="text-3xl text-center py-2 pt-5"> Ilustración </h1>
+
+        <div class="transition-all grid grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+            <Card
+            class="cursor-pointer w-50 h-75 text-white
+            sm:w-70 sm:h-85
+            md:w-80 md:h-100
+            xl:w-100 xl:h-110
+            "
+            v-for="obra in ilustracion"
+            >
+                <CardContent
+                class="flex flex-col items-center gap-1 w-full my-1 mx-1 "
+                >
+                    <div class="absolute w-50 h-50 mt-2 flex items-end justify-center
+                    hover:bg-black/40 hover:backdrop-blur-md text-white/0 hover:text-white
+                    transition-all
+                    sm:w-70 sm:h-70
+                    md:w-80 md:h-80
+                    xl:w-100 xl:h-100
+                    ">
+                        <p class="mb-5 xl:text-lg">{{ obra.fecha }}</p>
+                    </div>
+
+                    <img 
+                    class="object-contain mt-2 felx items-center"
+                    :src="`/imagenes/trabajos/${obra.grid}.jpg`" alt=""
+                    >
+
+                    <h2 class="font-medium text-lg lg:text-xl text-center pt-3"> {{ obra.titulo }} </h2>
+                </CardContent>
+            </Card>  
+        </div>
+
     </section>
 
-    <section id="comic" class="bg-blue-200 h-150">
-        <h1> Cómic </h1>
+    <section id="comic" class="bg-black text-white">
+       <h1 class="text-3xl text-center py-2 pt-5"> Cómic </h1>
+       
+        <div class="transition-all grid grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+            <Card
+            class="cursor-pointer w-50 h-75 text-white
+            sm:w-70 sm:h-85
+            md:w-80 md:h-100
+            xl:w-100 xl:h-110
+            "
+            v-for="obra in comic"
+            >
+                <CardContent
+                class="flex flex-col items-center gap-1 w-full my-1 mx-1 "
+                >
+                    <div class="absolute w-50 h-50 mt-2 flex items-end justify-center
+                    hover:bg-black/40 hover:backdrop-blur-md text-white/0 hover:text-white
+                    transition-all
+                    sm:w-70 sm:h-70
+                    md:w-80 md:h-80
+                    xl:w-100 xl:h-100
+                    ">
+                        <p class="mb-5 xl:text-lg">{{ obra.fecha }}</p>
+                    </div>
+
+                    <img 
+                    class="object-contain mt-2 felx items-center"
+                    :src="`/imagenes/trabajos/${obra.grid}.jpg`" alt=""
+                    >
+
+                    <h2 class="font-medium text-lg lg:text-xl text-center pt-3"> {{ obra.titulo }} </h2>
+                </CardContent>
+            </Card>  
+        </div>
     </section>
 
-    <section id="personajes" class="bg-emerald-200 h-200">
-        <h1> Diseño de personajes </h1>
+    <section id="personajes" class="bg-black text-white">
+        <h1 class="text-3xl text-center py-2 pt-10"> Diseño de personajes </h1>
+        
+        <div class="transition-all grid grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+            <Card
+            class="cursor-pointer w-50 h-75 text-white
+            sm:w-70 sm:h-85
+            md:w-80 md:h-100
+            xl:w-100 xl:h-110
+            "
+            v-for="obra in personajes"
+            >
+                <CardContent
+                class="flex flex-col items-center gap-1 w-full my-1 mx-1 "
+                >
+                    <div class="absolute w-50 h-50 mt-2 flex items-end justify-center
+                    hover:bg-black/40 hover:backdrop-blur-md text-white/0 hover:text-white
+                    transition-all
+                    sm:w-70 sm:h-70
+                    md:w-80 md:h-80
+                    xl:w-100 xl:h-100
+                    ">
+                        <p class="mb-5 xl:text-lg">{{ obra.fecha }}</p>
+                    </div>
+
+                    <img 
+                    class="object-contain mt-2 felx items-center"
+                    :src="`/imagenes/trabajos/${obra.grid}.jpg`" alt=""
+                    >
+
+                    <h2 class="font-medium text-lg lg:text-xl text-center pt-3"> {{ obra.titulo }} </h2>
+                </CardContent>
+            </Card>  
+        </div>
     </section>
 
-    <section id="diseno" class="bg-purple-200 h-200">
-        <h1> Diseño gráfico </h1>
+    <section id="diseno" class="bg-black text-white">
+        <h1 class="text-3xl text-center py-2 pt-10"> Diseño gráfico </h1>
+        
+        <div class="transition-all grid grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+            <Card
+            class="cursor-pointer w-50 h-75 text-white
+            sm:w-70 sm:h-85
+            md:w-80 md:h-100
+            xl:w-100 xl:h-110
+            "
+            v-for="obra in diseno"
+            >
+                <CardContent
+                class="flex flex-col items-center gap-1 w-full my-1 mx-1 "
+                >
+                    <div class="absolute w-50 h-50 mt-2 flex items-end justify-center
+                    hover:bg-black/40 hover:backdrop-blur-md text-white/0 hover:text-white
+                    transition-all
+                    sm:w-70 sm:h-70
+                    md:w-80 md:h-80
+                    xl:w-100 xl:h-100
+                    ">
+                        <p class="mb-5 xl:text-lg">{{ obra.fecha }}</p>
+                    </div>
+
+                    <img 
+                    class="object-contain mt-2 felx items-center"
+                    :src="`/imagenes/trabajos/${obra.grid}.jpg`" alt=""
+                    >
+
+                    <h2 class="font-medium text-lg lg:text-xl text-center pt-3"> {{ obra.titulo }} </h2>
+                </CardContent>
+            </Card>  
+        </div>
     </section>
 
-    <section id="otros" class="bg-red-200 h-200">
-        <h1> Otros </h1>
+    <section id="otros" class="bg-black text-white">
+        <h1 class="text-3xl text-center py-2 pt-10"> Otros </h1>
+        
+        <div class="transition-all grid grid-cols-2 lg:grid-cols-3 gap-8 place-items-center">
+            <Card
+            class="cursor-pointer w-50 h-75 text-white
+            sm:w-70 sm:h-85
+            md:w-80 md:h-100
+            xl:w-100 xl:h-110
+            "
+            v-for="obra in otros"
+            >
+                <CardContent
+                class="flex flex-col items-center gap-1 w-full my-1 mx-1 "
+                >
+                    <div class="absolute w-50 h-50 mt-2 flex items-end justify-center
+                    hover:bg-black/40 hover:backdrop-blur-md text-white/0 hover:text-white
+                    transition-all
+                    sm:w-70 sm:h-70
+                    md:w-80 md:h-80
+                    xl:w-100 xl:h-100
+                    ">
+                        <p class="mb-5 xl:text-lg">{{ obra.fecha }}</p>
+                    </div>
+
+                    <img 
+                    class="object-contain mt-2 felx items-center"
+                    :src="`/imagenes/trabajos/${obra.grid}.jpg`" alt=""
+                    >
+
+                    <h2 class="font-medium text-lg lg:text-xl text-center pt-3"> {{ obra.titulo }} </h2>
+                </CardContent>
+            </Card>  
+        </div>
     </section>
 
 </template>
